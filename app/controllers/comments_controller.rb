0b4b022ce4cc1583339baedfaae84a4bc1866bb1
haidang <dang.vohaidang@hcmut.edu.vn>
2023-07-21 09:49:48 +0700

    complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,28 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "haidang", password: "123456", only: [:destroy, :edit, :update]
     def create 
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: :see_other
+    end
+
+    def edit
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.update(comment_params)
+        redirect_to article_path(@article), status: :see_other
     end
 
     private
